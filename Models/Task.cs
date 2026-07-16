9e6f5741a192947a2ef979beeb943bb80b7fe26b
Cerenserbest <cerenserbest1234@gmail.com>
2026-07-16 14:20:31 +0300

    Görev veri modeli ve temel enum tanımları

---
+++ Models/Task.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text;
+using TaskManager.Enum;
+
+namespace TaskManager.Models
+{
+    internal class Task
+    {
+       public Guid Id;
+        public string Title;
+        public string Description;
+        public Priority Priority;
+		public Status Status;
+        public DateTime? DueDate;
+        public DateTime CreatedAt;
+        public DateTime? ComplatedAt;
+
+        public Task()
+        {
+            Id = Guid.NewGuid();
+			CreatedAt = DateTime.Now;
+            Status = Status.Pending;
+		}
+        }
+
+}
