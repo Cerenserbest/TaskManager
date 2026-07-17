681038ea9b5baf702cf6a55c6015f932f756d916
Cerenserbest <cerenserbest1234@gmail.com>
2026-07-17 10:58:05 +0300

    örev ekleme, listeleme, güncelleme ve filtreme işlevleri

---
+++ Exceptions/TaskValidationException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManager.Exceptions
+{
+    internal class TaskValidationException : Exception
+    {
+        public TaskValidationException(string message) : base(message)
+        { 
+
+        }
+    }
+}
--- Services/TaskService.cs
+++ Services/TaskService.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using Task = TaskManager.Models.Task;
+using TaskManager.Exceptions;
+using TaskManager.Enum;
 
 namespace TaskManager.Services
 {
@@ -9,29 +11,58 @@ namespace TaskManager.Services
     {
         private readonly List<Task>
             tasks = new List<Task>();
-       public void AddTask(Task task) {
-            if (string.IsNullOrWhiteSpace(task.Title) || task.Title.Length<3 || task.Title.Length > 100)
+        public void AddTask(Task task)
+        {
+            if (string.IsNullOrWhiteSpace(task.Title) || task.Title.Length < 3 || task.Title.Length > 100)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Hata: Başlık 3 ile 100 karakter arasında olmalıdır.");
-                Console.ResetColor();
-				return;
+                throw new TaskValidationException("Hata: Başlık boş olamaz ve 3 ile 100 karakter arasında olmalıdır.");
             }
-            if (task.DueDate != null && task.DueDate.Value < DateTime.Now ) 
+            if (task.DueDate != null && task.DueDate.Value < DateTime.Now)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Hata: Bitiş tarihi geçmiş bir tarih olamaz.");
-                Console.ResetColor();
-				return;
+                throw new TaskValidationException("Hata: Bitiş tarihi geçmiş bir tarih olamaz.");
             }
-            
-			tasks.Add(task);}
-		public IEnumerable<Task> GetAllTasks() {
+
+            tasks.Add(task);
+        }
+        public IEnumerable<Task> GetAllTasks()
+        {
             return tasks;
         }
-        public Task? GetTaskById(Guid id) {
-			return tasks.FirstOrDefault(t => t.Id == id);
-		}
+        public Task? GetTaskById(Guid id)
+        {
+            return tasks.FirstOrDefault(t => t.Id == id);
+        }
+        public void UpdateTask(Guid id, Task updatedTask)
+        {
+            var existingTask = GetTaskById(id);
+            if (GetTaskById(id) != null)
+            {
+                throw new TaskValidationException($"Hata: {id} id'li görev bulunamadı.");
+            }
+            existingTask.Title = updatedTask.Title;
+            existingTask.Description = updatedTask.Description;
+            existingTask.Priority = updatedTask.Priority;
+            existingTask.Status = updatedTask.Status;
+            existingTask.DueDate = updatedTask.DueDate;
+        }
+        public void DeleteTask(Guid id)
+        {
+            var taskToDelete = GetTaskById(id);
+            if (taskToDelete == null)
+            {
+                throw new TaskValidationException($"Hata: {id} id'li görev bulunamadı.");
+            }
+            tasks.Remove(taskToDelete);
+        }
+        public IEnumerable<Task> GetTasksByStatus(Status status)
+        {
+			return tasks.Where(t => t.Status == status);
+            
+        }
+        public IEnumerable<Task> GetTasksByPriority(Priority priority)
+        {
+			return tasks.Where(t => t.Priority == priority);
+            
+        }
     }
-
 }
